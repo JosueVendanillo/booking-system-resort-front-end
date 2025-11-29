c7fdec3df2c9c54eb518ebbc5a85117fd7fa1602
John Josue <josuevendanillo.jv@gmail.com>
2025-11-30 03:09:24 +0800

    put validation for the login form,  Feedbacks in the homepage are being fetched to the testimonials in homepage, cleanup logs

--- src/pages/admin/FeedbackManagement.jsx
+++ src/pages/admin/FeedbackManagement.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/feedback";
+const API_URL = "http://localhost:8080/api/feedbacks";
 
 function FeedbackManagement() {
   const [feedbacks, setFeedbacks] = useState([]);
